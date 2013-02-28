9382e25bc9e64ed6d27dbbc37ca2a850edd469b5
Cococoder <delaney.burke@me.com>
2013-02-28 14:50:46 +0000

    now with add device_type_id to devices removed the database prefix

---
+++ database/migrations/011.add_device_types_tabel.sql
@@ -0,0 +1,8 @@
+CREATE  TABLE `device_types` (
+  `id` INT NOT NULL ,
+  `name` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) );
+
+--//@UNDO
+
+DROP TABLE `device_types`
---
+++ database/migrations/013.add_foreign_key_device_type_id_to_devices.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `devices` ADD COLUMN `device_type_id` INT NOT NULL;
