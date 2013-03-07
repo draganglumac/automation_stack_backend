58c4688ac445ee958e1422a255675479a342de12
AlexsJones <alexsimonjones@gmail.com>
2013-03-07 10:18:29 +0000

    fixed broken procedure

--- database/migrations/008.set_job_status_from_id.sql
+++ database/migrations/008.set_job_status_from_id.sql
@@ -1,5 +1,5 @@
-CREATE PROCEDURE `set_job_status_from_id`(id int(11),status varchar(45))
-	UPDATE `jobs` SET `status` = status  WHERE `id` = id
+CREATE PROCEDURE `set_job_status_from_id`(machineid int(11),status varchar(45))
+	UPDATE `jobs` SET `status` = status  WHERE `id` = machineid
 	
 --//@UNDO
 
