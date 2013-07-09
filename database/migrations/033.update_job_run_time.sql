59cb9ba7b0484d7be6a6df05af4fec5ce0db425e
draganglumac <dragan.glumac@gmail.com>
2013-07-09 09:05:31 +0100

    Stored procedure for updating next run time and test for it.

--- database/migrations/033.update_job_run_time.sql
+++ database/migrations/033.update_job_run_time.sql
@@ -1,5 +1,5 @@
-CREATE PROCEDURE update_job_run_time(in job_id int(11))
-	UPDATE `jobs` SET `trigger_time` = @trigtime + @interval WHERE `id` = job_id;
+CREATE PROCEDURE `update_job_run_time`(in job_id int(11))
+	UPDATE `jobs` SET `trigger_time` = `trigger_time` + `interval` WHERE `id` = job_id;
 
 --//@UNDO
 
